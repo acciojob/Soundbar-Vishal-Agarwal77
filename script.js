d5e0cfab22e6ef30d5729daf6e6b0c93287cb985
Vishal-Agarwal77 <108511250+Vishal-Agarwal77@users.noreply.github.com>
2023-11-24 00:51:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+function playsound(soundfile){
+	const audio_ele=document.getElementById("audio");
+	while(audio_ele.firstChild){
+		audio_ele.removeChild(audio_ele.firstChild);
+	}
+	const audio_source=document.createElement('source');
+	audio_source.src=`sound/${soundfile}`;
+	audio_ele.appendChild(audio_source);
+	audio_ele.load();
+	audio_ele.play()
+	.catch((error)=>{
+		console.log(error);
+		alert("failed");
+	})
+}
+function stopsound() {
+	const audio_ele=document.getElementById("audio");
+	audio_ele.pause();
+	audio_ele.currentTime=0;
+}
